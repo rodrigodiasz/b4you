24d93d1c99ab516fb9c44b895840050ed6ffabcf
Rodrigo Oliveira <coderodrigo07@gmail.com>
2025-07-24 04:09:33 -0300

    Update api.ts

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -4,6 +4,8 @@ const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL as string,
 });
 
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
+
 api.interceptors.request.use((config) => {
   if (typeof window !== "undefined") {
     const token = localStorage.getItem("token");
